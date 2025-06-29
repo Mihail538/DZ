2912607cee38e91a4f20528917580dfcdf44c5b2
Mihail538 <misankinm5@gmail.com>
2025-06-29 18:47:30 +0200

    Add files via upload

---
+++ Untitled-2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a, p;
+    cout << "Poday bok";
+    cin >> a;
+    p = a * a;
+    cout << "Pole kwadratu P = " << p << endl;
+    
+    return 0;
+}
